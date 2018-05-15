fa5c4256461006e797b2b630ac5ab82dac0fee4d
Ozaren <krishna.sd.2012@gmail.com>
2018-05-15 17:32:39 -0400

    begin change to vectorizing the input

--- src/noise.rs
+++ src/noise.rs
@@ -1,5 +1,5 @@
 use rand::{Rng, ThreadRng, thread_rng};
-use vector::vector::VectorD;
+use vector::vector::{VectorD, VectorS};
 use std::f64;
 
 fn lerp(v1: &VectorD, v2: &VectorD, w: f64) -> Result<VectorD, String> {
@@ -16,18 +16,21 @@ pub enum NoiseType {
 /// struct to handle generating perlin noise
 pub struct PerlinNoise {
     rng: ThreadRng, noise_type: NoiseType,
-    grad: Vec<Vec<(VectorD, VectorD)>>,
-    offsets: Vec<Vec<VectorD>>,
-    dim: usize, width: usize, height: usize
+    grad: Vec<Vec<VectorD>>,
+    offsets: Vec<VectorD>,
+    in_dim: usize, out_dim: usize, 
+    bounds: VectorS
 }
 
 impl PerlinNoise {
-    pub fn new(noise_type: NoiseType, dim: usize, width: usize, height: usize) -> Self {
+    pub fn new(noise_type: NoiseType, in_dim: usize, out_dim: usize, bounds: VectorS) -> Self {
+        println!("currently in_dim is not utilized and is force set to 2");
         let mut noise = Self {
             rng: thread_rng(),
             grad: Vec::new(),
             offsets: Vec::new(),
-            noise_type, dim, width, height
+            in_dim: 2, out_dim,
+            noise_type, bounds
         };
         let _ = noise.regen();
         noise
@@ -35,91 +38,136 @@ impl PerlinNoise {
 }
 
 impl PerlinNoise {
+    fn cumulative_bounds(&self) -> Vec<usize> {
+        let mut v = vec![];
+
+        for i in 0..self.bounds.dim() {
+            if i == 0 {
+                v.push(1);
+            } else {
+                let x = *v.last().unwrap();
+                v.push(x * self.bounds[i]);
+            }
+        }
+
+        v
+    }
+
+    fn linearize(&self, x: &VectorS) -> usize {
+        VectorS::from(self.cumulative_bounds()).dot(x)
+    }
+    
+    fn vectorize(&self, mut p: usize) -> VectorS {
+        let mut v = self.cumulative_bounds();
+        let mut pos = vec![];
+
+        while let Some(x) = v.pop() {
+            let (a, b) = (p / x, p  % x);
+            p = b;
+            pos.insert(0, a);
+        }
+
+        VectorS::from(pos)
+    }
+
     /// regenerate the random weights
     pub fn regen(&mut self) -> Result<(), String> {
         self.grad = Vec::new();
         self.offsets = Vec::new();
-        let origin = VectorD::new(self.dim);
+        let origin = VectorD::new(self.out_dim);
+        let last_index = self.cumulative_bounds().pop().unwrap();
 
-        for x in 0..self.width+1 {
-            self.grad.push(Vec::new());
-            self.offsets.push(Vec::new());
-            for y in 0..self.height+1 {
-                match self.noise_type {
-                    NoiseType::Perlin => {
-                        let mut t = Vec::new();
-                        for _ in 0..self.dim {
-                            t.push(VectorD::rand(2));
+        for x in 0..last_index {
+            match self.noise_type {
+                NoiseType::Perlin => {
+                    let mut t = Vec::new();
+                    for _ in 0..self.out_dim {
+                        t.push(VectorD::rand(2));
+                    }
+                    let mut t0 = VectorD::new(self.out_dim);
+                    let mut t1 = VectorD::new(self.out_dim);
+                    let mut tt = Vec::new();
+
+                    for j in 0..self.in_dim {
+                        let mut v = Vec::new();
+                        for i in 0..self.out_dim {
+                            v.push(t[i][j]);
                         }
-                        let mut t0 = VectorD::new(self.dim);
-                        let mut t1 = VectorD::new(self.dim);
+                        tt.push(VectorD::from(v));
+                    }
 
-                        for i in 0..self.dim {
-                            t0[i] = t[i][0];
-                            t1[i] = t[i][1];
-                        }
+                    self.grad[x] = tt;
+                    self.offsets[x] = origin.clone();
+                },
+                NoiseType::Barycentric => {
+                    let t = VectorD::rand(self.out_dim);
 
-                        self.grad[x][y] = (t0, t1);
-                        self.offsets[x][y] = origin.clone();
-                    },
-                    NoiseType::Barycentric => {
-                        let t = VectorD::rand(self.dim);
-
-                        let mut max_l = f64::INFINITY;
-                        let mut min_l = f64::NEG_INFINITY;
-                        
-                        // Find the range of values for l such that
-                        // (l * t + simplexCenter) is still inside the 
-                        // simplex. Recall the simplex is bounded by hyperplane
-                        // x_i >= 0, so this is easy to calculate.
-                        for i in 0..self.dim {
-                            if t[i] != 0.0 {
-                                let l = -1.0 / (self.dim as f64 * t[i]);
-                                if l < 0.0 {
-                                    min_l = f64::max(min_l, l);
-                                } else {
-                                    max_l = f64::min(max_l, l);
-                                }
+                    let mut max_l = f64::INFINITY;
+                    let mut min_l = f64::NEG_INFINITY;
+                    
+                    // Find the range of values for l such that
+                    // (l * t + simplexCenter) is still inside the 
+                    // simplex. Recall the simplex is bounded by hyperplane
+                    // x_i >= 0, so this is easy to calculate.
+                    for i in 0..self.out_dim {
+                        if t[i] != 0.0 {
+                            let l = -1.0 / (self.out_dim as f64 * t[i]);
+                            if l < 0.0 {
+                                min_l = f64::max(min_l, l);
+                            } else {
+                                max_l = f64::min(max_l, l);
                             }
                         }
-                        if max_l < min_l {
-                            return Err(format!("invalid VectorDs"));
-                        }
-                        // Given min_l and max_l, we compute how to 
-                        // scale and offset the gradient so that the range
-                        // matches min_l and max_l
-                        let center = (min_l + max_l) / 2.0;
-                        let half_width = (max_l - min_l) / 2.0;
-                        let offset = (&t * center).shift(1.0 / self.dim as f64);
-                        let t = t * half_width;
-
-                        // Same as BARYCENTRIC_VARIANT
-                        let angle = self.rng.gen::<f64>() * 2.0 * f64::consts::PI;
-                        let s = angle.sin();
-                        let c = angle.cos();
-                        self.grad[x][y] = (&t * s, &t * c);
-                        self.offsets[x][y] = offset;
                     }
+                    if max_l < min_l {
+                        return Err(format!("invalid vectors"));
+                    }
+                    // Given min_l and max_l, we compute how to 
+                    // scale and offset the gradient so that the range
+                    // matches min_l and max_l
+                    let center = (min_l + max_l) / 2.0;
+                    let half_width = (max_l - min_l) / 2.0;
+                    let offset = (&t * center).shift(1.0 / self.out_dim as f64);
+                    let t = t * half_width;
+
+                    let dir = VectorD::rand(self.in_dim);
+                    let mut grad_x = Vec::new();
+
+                    for i in 0..self.in_dim {
+                        grad_x.push(&t * dir[i])
+                    }
+                    
+                    self.grad[x] = grad_x;
+                    self.offsets[x] = offset;
                 }
             }
         }
+
         Ok(())
     }
 
+    fn apply_grad(&self, ix: VectorD, x: VectorD) -> Result<VectorD, String> {
+        let pos: VectorS = ix.convert(|x| x as usize);
+        let delta = &x - &ix;
+        delta
+    }
+
+    /*
     fn apply_grad(&self, ix: usize, iy: usize, x: f64, y: f64) -> Result<VectorD, String> {
         let (dx, dy) = (x - ix as f64, y - iy as f64);
         let (dx, dy) = (dx as f64, dy as f64);
         let g = &self.grad[ix][iy];
         &self.offsets[ix][iy] + &(&g.0 * dx + &g.1 * dy)?
     }
+    */
 
     /// evaluate the noise function at a point
     pub fn eval(&self, x: f64, y: f64) -> VectorD {
         // Determine grid cell coordinates
-        let x0 = x.floor() as usize;
-        let x1 = x0 + 1;
-        let y0 = y.floor() as usize;
-        let y1 = y0 + 1;
+        let x0 = x.floor();
+        let x1 = x0 + 1.0;
+        let y0 = y.floor();
+        let y1 = y0 + 1.0;
     
         // Determine interpolation weights
         // Could also use higher order polynomial/s-curve here
@@ -127,12 +175,12 @@ impl PerlinNoise {
         let sy = y - y0 as f64;
     
         // Interpolate between grid point gradients
-        let n0 = self.apply_grad(x0, y0, x, y).unwrap();
-        let n1 = self.apply_grad(x1, y0, x, y).unwrap();
+        let n0 = self.apply_grad(VectorD::from(vec![x0, y0]), VectorD::from(vec![x, y])).unwrap();
+        let n1 = self.apply_grad(VectorD::from(vec![x1, y0]), VectorD::from(vec![x, y])).unwrap();
         let ix0 = lerp(&n0, &n1, sx).unwrap();
 
-        let n0 = self.apply_grad(x0, y1, x, y).unwrap();
-        let n1 = self.apply_grad(x1, y1, x, y).unwrap();
+        let n0 = self.apply_grad(VectorD::from(vec![x0, y1]), VectorD::from(vec![x, y])).unwrap();
+        let n1 = self.apply_grad(VectorD::from(vec![x1, y1]), VectorD::from(vec![x, y])).unwrap();
         let ix1 = lerp(&n0, &n1, sx).unwrap();
         
         let value = lerp(&ix0, &ix1, sy).unwrap();
