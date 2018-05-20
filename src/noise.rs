c9e9eddcb0f6a5745ecabb4da2ed237d472f8f1d
Ozaren <krishna.sd.2012@gmail.com>
2018-05-20 11:03:53 -0400

    updated version number added a range feature,     specifies the range of the noise (0 to 1) or (-1 to 1)

--- src/noise.rs
+++ src/noise.rs
@@ -4,6 +4,20 @@ use num::traits::*;
 use rand::{Rng, Rand, thread_rng};
 use linear_algebra::vector::{Vector, VectorS};
 
+#[derive(Clone, Copy)]
+pub enum Mode {
+    Debug, Release
+}
+
+macro_rules! ifdebug {
+    ($on: expr => $thing: expr) => {
+        let mode: Mode = $on;
+        if let Mode::Debug = mode {
+            $thing;
+        }
+    };
+}
+
 fn lerp<T>(v1: &Vector<T>, v2: &Vector<T>, w: T) -> Result<Vector<T>, String>
 where T: Float {
     let a = T::from(6.0).unwrap();
@@ -19,6 +33,11 @@ pub enum NoiseType {
     Perlin, Barycentric
 }
 
+pub enum Range {
+    Pos,
+    PosNeg
+}
+
 /// struct to handle generating perlin noise
 pub struct PerlinNoise<T>
     where T: Copy + Clone + Rand + Float {
@@ -26,17 +45,21 @@ pub struct PerlinNoise<T>
     grad: Vec<Vec<Vector<T>>>,
     offsets: Vec<Vector<T>>,
     in_dim: usize, out_dim: usize, 
-    bounds: VectorS
+    bounds: VectorS, mode: Mode,
+    range: Range,
+    zero: T, one: T, two: T
 }
 
 impl<T> PerlinNoise<T>
     where T: Copy + Clone + Rand + Float {
-    pub fn new(noise_type: NoiseType, in_dim: usize, out_dim: usize, bounds: VectorS) -> Result<Self, String> {
+    pub fn new(noise_type: NoiseType, in_dim: usize, out_dim: usize, bounds: VectorS, mode: Mode, range: Range) -> Result<Self, String> {
         let mut noise = Self {
             grad: Vec::new(),
             offsets: Vec::new(),
             in_dim, out_dim,
-            noise_type, bounds
+            noise_type, bounds,
+            mode, range, zero: T::zero(),
+            one: T::one(), two: (T::one() + T::one())
         };
         noise.regen()?;
         Ok(noise)
@@ -71,7 +94,7 @@ impl<T> PerlinNoise<T>
         let mut pos = vec![];
 
         while let Some(x) = v.pop() {
-            let (a, b) = (p / x, p  % x);
+            let (a, b) = (p / x, p % x);
             p = b;
             pos.insert(0, a);
         }
@@ -83,23 +106,26 @@ impl<T> PerlinNoise<T>
         let mut rng = thread_rng();
         let n = self.out_dim;
 
-        let zero = T::zero();
-        let one = T::one();
-        let two = one + one;
+        let counter = 0usize;
         
         loop {
+            ifdebug! (self.mode => {
+                if counter % 1000 == 0 {
+                    println!("rand finder = {}", counter)
+                }
+            });
             let mut v = Vec::new();
-            let mut sum = zero;
+            let mut sum = self.zero;
 
             for _ in 0..n-1 {
-                let x = rng.gen::<T>() * two - one;
+                let x = rng.gen::<T>() * self.two - self.one;
                 v.push(x);
                 sum = sum + x;
             }
 
-            v.push(zero - sum);
+            v.push(self.zero - sum);
             let v = Vector::from(v);
-            if v.magsq() <= one {
+            if v.magsq() <= self.one {
                 return Vector::from(v).norm();
             }
         }
@@ -108,20 +134,27 @@ impl<T> PerlinNoise<T>
 
     /// regenerate the random weights
     pub fn regen(&mut self) -> Result<(), String> {
-        self.grad = Vec::new();
-        self.offsets = Vec::new();
-        let origin = Vector::new(self.out_dim);
         let last_index = self.cumulative_bounds().pop().unwrap() + 1;
+        self.grad = Vec::with_capacity(last_index);
+        self.offsets = Vec::with_capacity(last_index);
+        let origin = Vector::new(self.out_dim);
+        let perc = (last_index as f64 * 0.01) as usize + 1;
 
-        for _ in 0..last_index {
-            match self.noise_type {
-                NoiseType::Perlin => {
+        ifdebug!(self.mode => println!("start regen"));
+
+        
+        match self.noise_type {
+            NoiseType::Perlin => {
+                ifdebug!(self.mode => println!("using perlin noise"));
+                for i in 0..last_index {
                     let mut t = Vec::new();
+
                     for _ in 0..self.out_dim {
-                        t.push(Vector::rand(2));
+                        t.push(Vector::rand(self.in_dim));
                     }
                     let mut tt = Vec::new();
 
+                    ifdebug!(self.mode => if i % perc == 0 { println!("generating gradient {} of {}", i, last_index) } );
                     for j in 0..self.in_dim {
                         let mut v = Vec::new();
                         for i in 0..self.out_dim {
@@ -132,23 +165,28 @@ impl<T> PerlinNoise<T>
 
                     self.grad.push(tt);
                     self.offsets.push(origin.clone());
-                },
-                NoiseType::Barycentric => {
+                }
+            },
+            NoiseType::Barycentric => {
+                ifdebug!(self.mode => println!("using barycentric noise"));
+                let out_dim = T::from(self.out_dim).unwrap();
+                let one = T::one();
+                let two = one + one;
+
+                for i in 0..last_index {
                     let t = self.random_bary_vec();
 
                     let mut max_l = T::infinity();
                     let mut min_l = T::neg_infinity();
-                    let out_dim = T::from(self.out_dim).unwrap();
-                    let one = T::one();
-                    let two = one + one;
                     
                     // Find the range of values for l such that
                     // (l * t + simplexCenter) is still inside the 
                     // simplex. Recall the simplex is bounded by hyperplane
                     // x_i >= 0, so this is easy to calculate.
+                    ifdebug!(self.mode => if i % perc == 0 { println!("generating gradient {} of {}", i, last_index) } );
                     for i in 0..self.out_dim {
                         if !t[i].is_zero() {
-                            let l = one.neg() / (out_dim * t[i]);
+                            let l = self.one.neg() / (out_dim * t[i]);
                             if l.is_sign_negative() {
                                 min_l = min_l.max(l);
                             } else {
@@ -178,7 +216,7 @@ impl<T> PerlinNoise<T>
                     self.offsets.push(offset);
                 }
             }
-        }
+        };
 
         Ok(())
     }
@@ -204,10 +242,10 @@ impl<T> PerlinNoise<T>
         if level > 0 {
             let level = level - 1;
             
-            del[level] = T::zero();
+            del[level] = self.zero;
             let n0 = self.eval_rec(v, v0, del, level)?;
             
-            del[level] = T::one();
+            del[level] = self.one;
             let n1 = self.eval_rec(v, v0, del, level)?;
 
             lerp(&n0, &n1, v[level] - v0[level])
@@ -217,6 +255,10 @@ impl<T> PerlinNoise<T>
     }
 
     pub fn eval(&self, v: &Vector<T>) -> Result<Vector<T>, String> {
-        self.eval_rec(&v, &v.map(|&x| x.floor()), &mut vectorize![T::zero(); v.dim()], v.dim())
+        let eval = self.eval_rec(&v, &v.map(|&x| x.floor()), &mut vectorize![self.zero; v.dim()], v.dim())?;
+        Ok(match self.range {
+            Range::Pos => eval.shift(self.one),
+            Range::PosNeg => eval * self.two
+        })
     }
 }
